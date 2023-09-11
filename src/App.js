cc0f3afc891ce33cd7aec8e329aa8a55c5b2e9df
DESKTOP-L9U2CF5\HARR <hpzhd.hp@gmail.com>
2023-09-12 02:30:16 +0530

    final commit 2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import react, { useState } from "react";
+import { useState } from "react";
 import Expenses from "./Components/Expenses/Expenses";
 import NewExpense from "./Components/NewExpense/NewExpense";
 
